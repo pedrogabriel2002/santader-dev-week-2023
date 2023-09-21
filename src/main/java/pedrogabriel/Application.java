1444bfe182370a5ce63a94fcb4e60f96ef48b09f
Pedro Gabriel <bielpedro300@gmail.com>
2023-09-21 12:29:10 -0300

    Fix NetworkError

--- src/main/java/pedrogabriel/Application.java
+++ src/main/java/pedrogabriel/Application.java
@@ -3,6 +3,11 @@ package pedrogabriel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+
+@OpenAPIDefinition(servers = { @Server(url="/", description = "default server url")})
 @SpringBootApplication
 public class Application {
 
